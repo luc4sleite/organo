9c2bbad1ca3b12efd4658f232c4629aec785a6a5
luc4sleite <lucahleite@gmail.com>
2023-04-10 11:06:13 -0300

    feat: card do colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,18 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'>
+            <div className='cabecalho'>
+                <img src='https://github.com/luc4sleite.png' alt='Lucas Leite'/>
+            </div>
+            <div className='rodape'>
+                <h4>Lucas Leite</h4>
+                <h5>Instrutor</h5>
+
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
